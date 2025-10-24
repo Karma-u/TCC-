a0996679d1841ea2435473e505386625225dba52
Karma-u <135573405+Karma-u@users.noreply.github.com>
2025-10-24 08:36:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,18 @@
     }
 
     menuBtn.addEventListener("click", toggleMenu);
+
     overlay.addEventListener("click", toggleMenu);
+
+const searchInput = document.getElementById('searchInput');
+    const gameList = document.getElementById('gameList');
+    const games = gameList.getElementsByTagName('li');
+
+    searchInput.addEventListener('input', () => {
+      const filtro = searchInput.value.toLowerCase();
+
+      for (let jogo of games) {
+        const nome = jogo.textContent.toLowerCase();
+        jogo.classList.toggle('hidden', !nome.includes(filtro));
+      }
+    });
